3fc67d2cceaf35354c4ceea8b7a1a34b6858d376
roman012285 <57875954+roman012285@users.noreply.github.com>
2022-10-10 15:37:36 +0300

    Create ex1.cpp

---
+++ lesson5/ex1.cpp
@@ -0,0 +1,23 @@
+/* Lesson 5 ex1
+   Email : roman329@gmail.com
+ */
+
+#include <iostream>
+using namespace std;
+
+// Function prototypes
+void print();
+
+int main() {
+	
+	print();
+
+	return 0;
+
+}
+
+void print() {
+
+	cout << "Hello I am a function" << endl;
+
+}
