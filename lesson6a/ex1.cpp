eaa166939b8d10323e73242ba750454e045ad150
roman012285 <57875954+roman012285@users.noreply.github.com>
2022-10-29 16:51:54 +0300

    Create ex1.cpp

---
+++ lesson6a/ex1.cpp
@@ -0,0 +1,17 @@
+/*Lesson 6 ex1a
+* Roman Gilgor
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a[] = { 1, -300, 5, 7};
+
+	for (int i = 0; i < 4; i++)
+		cout << a[i] << "    ";
+
+	return 0;
+}
+
