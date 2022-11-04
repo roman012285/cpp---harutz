99a37617c3177b0bc808a77a2d3e75bf33688cb9
roman012285 <57875954+roman012285@users.noreply.github.com>
2022-11-04 17:49:59 +0200

    Create ex1.cpp

---
+++ lesson6c/ex1.cpp
@@ -0,0 +1,42 @@
+/*
+	String Ex1
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string reverse(string s);
+
+int main() {
+	
+	string s;
+	string s_reverse;
+
+	cout << "Please Enter your string" << endl;
+	getline(cin, s);
+	
+	s_reverse = reverse(s);
+
+	cout << s_reverse;
+
+	return 0;
+}
+
+string reverse(string s) {
+
+	char temp;
+	int len = s.length();
+
+	int eff_len = len / 2;
+
+	for (int i = 0; i < eff_len; i++) {
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+	}
+
+	return s;
+
+}
